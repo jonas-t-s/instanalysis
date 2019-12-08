a969f80f02ce9ea9e0287f3efb8b53dca1c44f08
MelcomX <jonas.till.schmid@gmail.com>
2019-12-08 21:09:24 +0100

    Update missing_followers.py
    Changed according to the suggestions.

--- scripts/python/missing_followers.py
+++ scripts/python/missing_followers.py
@@ -3,7 +3,7 @@ import sys
 
 if len(sys.argv) == 3:
  try:
-    print "Old:", sys.argv[1]
+    print "Old:",sys.argv[1]
     print "New:", sys.argv[2]
     json_file_old = open(sys.argv[1])
     connections_old = json.load(json_file_old)
@@ -16,28 +16,26 @@ if len(sys.argv) == 3:
     for user in connections_new['followers']:
         follower_new.append(user)
 
-    found = None
-    overall = None
+    found_unfollower = False
+    found_overall = False
     print("Users that followed you before, but now stopped to follow you:")
     for a, val in enumerate(follower_old):
-	for b, val in enumerate(follower_new):
-		#print b
-		if follower_old[a]==follower_new[b]:
-			found = True
-			break
-	if not found:
-		overall = True
-		print follower_old[a]
-		continue
-	else:
-		found = None
+        for b, val in enumerate(follower_new):
+                if follower_old[a]==follower_new[b]:
+                        found_unfollower = True
+                        break
+        if not found_unfollower:
+                found_overall = True
+                print follower_old[a]
+                continue
+        else:
+                found_unfollower = False
 
-    if not overall:
-	print("It looks like your friends like you. Nobody has unfollowed you!")
+    if not found_overall:
+        print("It looks like your friends like you. Nobody has unfollowed you!")
  except FileNotFoundError:
       sys.exit("At least one of your files does not exist")
  except IOError: 
       sys.exit("At least one of your files is corrupt or doesn't exist")
-
 else:
     print('correct usage: python missing_followers.py <connections_old.json> <connections_new.json>')
