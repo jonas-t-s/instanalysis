f3d6d2b8096f0d5c6c7870af2024ae373631db86
MelcomX <jonas.till.schmid@gmail.com>
2020-06-26 13:56:37 +0200

    Updated to Python 3.8

--- scripts/python/missing_followers.py
+++ scripts/python/missing_followers.py
@@ -4,8 +4,8 @@ import sys
 # from jsondiff import diff
 
 if len(sys.argv) == 3:
-    print "Old:", sys.argv[1]
-    print "New:", sys.argv[2]
+    print("Old:", sys.argv[1])
+    print("New:", sys.argv[2])
     json_file_old = open(sys.argv[1])
     connectionsold = json.load(json_file_old)
     json_file_new = open(sys.argv[2])
@@ -21,19 +21,19 @@ if len(sys.argv) == 3:
     overall = None
     print("Users that followed you before, but now stopped to follow you:")
     for a, val in enumerate(followerold):
-	for b, val in enumerate(followernew):
-		#print b
-		if followerold[a]==followernew[b]:
-			found = True
-			break
-	if not found:
-		overall = True
-		print followerold[a]
-		continue
-	else:
-		found = None
+        for b, val in enumerate(followernew):
+                #print b
+                if followerold[a]==followernew[b]:
+                        found = True
+                        break
+        if not found:
+                overall = True
+                print(followerold[a])
+                continue
+        else:
+                found = None
 
     if not overall:
-	print("It looks like your friends like you. There is no unfollower!")
+        print("It looks like your friends like you. There is no unfollower!")
 else:
     print('correct usage: python missing_followers.py <connections_old.json> <connections_new.json>')
