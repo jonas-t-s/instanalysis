f1e139de0cac46670c9321d26beb02207722fcd6
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2023-06-03 12:07:21 +0200

    Updated to fit newest archive state

--- scripts/python/follfileinterpreter.py
+++ scripts/python/follfileinterpreter.py
@@ -1,12 +1,14 @@
 import json
 import sys
-
+import zipfile
 
 def getusername(user):
     return(user['string_list_data'][0]['value'])
 def followerfileinterpreter(followersjsonfile):
     followers = []
-    for user in followersjsonfile['relationships_followers']:
+    if "relationships_followers" in followersjsonfile:
+        followersjsonfile = followersjsonfile["relationships_followers"]
+    for user in followersjsonfile:
         followers.append(getusername(user))
     return followers
 def followingfileinterpreter(followingfile):
@@ -14,3 +16,12 @@ def followingfileinterpreter(followingfile):
     for user in followingfile['relationships_following']:
         following.append(getusername(user))
     return following
+
+def main():
+    oldfiles = input("Where is the old downloaded archive located?")
+    newfiles = input("Where is the new downloaded archive located?")
+    with zipfile.ZipFile(oldfiles, 'r'), zipfile.ZipFile(newfiles, 'r') as oldfile, newfile:
+        print(oldfile.read())
+        print(newfile.read())
+if __name__ == "__main__":
+    main()
